07b0f66ef8ec538a75ad773c235d2d983c8f779c
Max Mynter <maxmynter@me.com>
2024-12-29 20:04:34 +0100

    Clean up entropies folder

--- src/te_toolbox/entropies.py
+++ src/te_toolbox/entropies.py
@@ -1,3 +1,5 @@
+"""Contains all the entropy and derived measures."""
+
 import numpy as np
 import numpy.typing as npt
 
@@ -6,17 +8,20 @@ def entropy(
     data: npt.NDArray[np.float64],
     bins: int | list[int] | npt.NDArray[np.float64],
 ):
-    """Calculate the entropy of one or more datasets
+    """Calculate the entropy of one or more datasets.
 
     Args:
+    ----
         data: Input data array. Can be 1D or 2D [timesteps x variables].
         bins: Number of bins for histogram or list of bin edges.
 
     Returns:
+    -------
         float: Entropy value for 1D input
         ndarray: Array of entropy values for 2D input, one per variable
 
     Raises:
+    ------
         ValueError: If data dimensions are invalid
 
     """
@@ -25,14 +30,18 @@ def entropy(
 
     match data.ndim:
         case 1:
-            hist, _ = np.histogram(data, bins = bins) 
+            hist, _ = np.histogram(data, bins=bins)
             hist = hist / len(data)
-            hist = np.where(hist==0, 1, hist)
+            hist = np.where(hist == 0, 1, hist)
             return -np.sum(hist * np.log(hist))
         case 2:
             n_vars = data.shape[1]
-            if isinstance(bins, (int, float)):
-                bins = [bins]* n_vars
+            if isinstance(bins, int | float):
+                bins = [bins] * n_vars
             return np.array([entropy(data[:, i], bins[i]) for i in range(n_vars)])
         case _:
-            raise ValueError("Wrong data format. Data must be of dimension [timesteps] or [timesteps x variables]")
+            raise ValueError(
+                """
+                Wrong data format.
+                Data must be of dimension [timesteps] or [timesteps x variables]"""
+            )
