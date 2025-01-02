6e706480d2194527b36bcf7ae785acfbe8cfc8e2
Max Mynter <maxmynter@me.com>
2025-01-02 12:58:14 +0100

    Expose new entropy function on package root

--- src/te_toolbox/__init__.py
+++ src/te_toolbox/__init__.py
@@ -1,5 +1,19 @@
 """Transfer entropy and information theory research utilities package."""
 
-from .entropies import entropy
+from .entropies import (
+    conditional_entropy,
+    entropy,
+    joint_entropy,
+    logn_normalized_transfer_entropy,
+    normalized_transfer_entropy,
+    transfer_entropy,
+)
 
-__all__ = ["entropy"]
+__all__ = [
+    "conditional_entropy",
+    "entropy",
+    "joint_entropy",
+    "logn_normalized_transfer_entropy",
+    "normalized_transfer_entropy",
+    "transfer_entropy",
+]
