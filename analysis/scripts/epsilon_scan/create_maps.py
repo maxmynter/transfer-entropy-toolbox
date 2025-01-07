cd294bb59c957b65bb1523379b8294cf33e99e9e
Max Mynter <maxmynter@me.com>
2025-01-07 17:32:36 +0100

    Update Lattice Generation Parameters to thesis values

--- analysis/scripts/epsilon_scan/create_maps.py
+++ analysis/scripts/epsilon_scan/create_maps.py
@@ -15,9 +15,9 @@ from te_toolbox.systems.maps import (
 
 OUTPUT_DIR = Path("data/")
 SEED = 42
-N_TRANSIENT = 10
-N_MAPS = 10
-N_ITER = 30
+N_TRANSIENT = 10**4
+N_MAPS = 100
+N_ITER = 300
 LAG = 1
 EPSILONS = np.linspace(0, 1, 20)
 N_BINS = np.arange(2, 31)
