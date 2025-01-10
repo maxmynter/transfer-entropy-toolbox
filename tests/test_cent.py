32138cbd06c12952a0538fce2d819e6d650d5513
Max Mynter <maxmynter@me.com>
2025-01-10 11:04:07 +0100

    Add Probing tests for conditional entropy
    i.e. calculating it manually for specific distributions and check-
    ing the results.

--- tests/test_cent.py
+++ tests/test_cent.py
@@ -56,3 +56,51 @@ def test_cent_error_handling():
     # Test empty input
     with pytest.raises(ValueError):
         conditional_entropy(np.array([[]]), bins=3)
+
+
+def test_conditional_entropy_known_distribution():
+    """Test conditional entropy with independent variables."""
+    x = np.array([0, 0, 1, 1])
+    y = np.array([0, 1, 0, 1])
+    data = np.column_stack([x, y])
+    result = conditional_entropy(data, bins=2)
+    assert_almost_equal(result[1, 0], np.log(2))  # H(Y|X)
+    assert_almost_equal(result[0, 1], np.log(2))  # H(X|Y)
+    assert_almost_equal(result[0, 0], 0.0)  # H(X|X)
+    assert_almost_equal(result[1, 1], 0.0)  # H(Y|Y)
+
+
+def test_cent_with_dependent_distribution():
+    """Test conditional entropy with completely dependent variables."""
+    x = np.array([0, 1, 0, 1])
+    y = np.array([0, 1, 0, 1])
+    data = np.column_stack([x, y])
+    result = conditional_entropy(data, bins=2)
+    assert_almost_equal(result[1, 0], 0.0)  # H(Y|X)
+    assert_almost_equal(result[0, 1], 0.0)  # H(X|Y)
+    assert_almost_equal(result[0, 0], 0.0)  # H(X|X)
+    assert_almost_equal(result[1, 1], 0.0)  # H(Y|Y)
+
+
+def test_cent_with_partial_dependency():
+    """Test conditional entropy with partially dependent variable.
+
+    X = [0,0,1,1], Y = [0,0,0,1]
+    P(Y=0|X=0) = 1    P(Y=1|X=0) = 0
+    P(Y=0|X=1) = 0.5  P(Y=1|X=1) = 0.5
+    H(Y|X) = 1/2 * log(2)
+
+    P(X=0|Y=0) = 2/3  P(X=1|Y=0) = 1/3
+    P(X=0|Y=1) = 0    P(X=1|Y=1) = 1
+    H(X|Y) = 3/4 * (-2/3 * log(2/3) - 1/3 * log(1/3))
+    """
+    x = np.array([0, 0, 1, 1])
+    y = np.array([0, 0, 0, 1])
+    data = np.column_stack([x, y])
+    result = conditional_entropy(data, bins=2)
+    y_given_x = 1 / 2 * np.log(2)
+    x_given_y = 3 / 4 * (-2 / 3 * np.log(2 / 3) - 1 / 3 * np.log(1 / 3))
+    assert_almost_equal(result[1, 0], y_given_x)  # H(Y|X)
+    assert_almost_equal(result[0, 1], x_given_y)  # H(X|Y)
+    assert_almost_equal(result[0, 0], 0.0)  # H(X|X)
+    assert_almost_equal(result[1, 1], 0.0)  # H(Y|Y)
