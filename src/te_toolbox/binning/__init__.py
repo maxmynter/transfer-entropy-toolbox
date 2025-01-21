758c79a1e88ed0e8fd01d9c6f81f8f8dadf687e9
Max Mynter <maxmynter@me.com>
2025-01-21 16:04:58 +0100

    (binning) Delete clustering based binning methods
    they don't really make sense in our analysis framework as we work
    with continuous data with no clear class boundaries. So using these
    methods yields poor results that are expected because the methods
    are not suitable for this usecase. I will exclude them from further
    evaluation. (they performed expectedly poor on the tests i did).

--- src/te_toolbox/binning/__init__.py
+++ src/te_toolbox/binning/__init__.py
@@ -1,7 +1,5 @@
 """Discretization and binning methods and utilities."""
 
-from importlib.util import find_spec
-
 from te_toolbox.binning.rules import (
     doanes_bins,
     freedman_diaconis_bins,
@@ -18,28 +16,12 @@ from te_toolbox.binning.statistical import (
     small_sample_akaike_bins,
 )
 
-HAS_SKLEARN = find_spec("sklearn") is not None
-
-
-if HAS_SKLEARN:
-    from te_toolbox.binning.clustering import (
-        agglomerative_bins,
-        dbscan_bins,
-        kmeans_bins,
-        meanshift_bins,
-    )
-
 __all__ = [
-    "HAS_SKLEARN",
-    "agglomerative_bins",
     "aic_bins",
     "bic_bins",
-    "dbscan_bins",
     "doanes_bins",
     "freedman_diaconis_bins",
-    "kmeans_bins",
     "knuth_bins",
-    "meanshift_bins",
     "rice_rule_bins",
     "scotts_rule_bins",
     "shimazaki_bins",
