1f53558a00b4c2af898410ed76e24f8555498a2d
Max Mynter <maxmynter@me.com>
2025-01-28 16:35:46 +0100

    Expose Conditional Entropy in Fast Entropy

--- src/te_toolbox/fast_entropy/__init__.py
+++ src/te_toolbox/fast_entropy/__init__.py
@@ -1,12 +1,14 @@
 """Package for fast entropy calculations implemented in C++."""
 
 from ._fast_entropy import (
+    discrete_conditional_entropy,
     discrete_entropy,
     discrete_joint_entropy,
     discrete_multivar_joint_entropy,
 )
 
 __all__ = [
+    "discrete_conditional_entropy",
     "discrete_entropy",
     "discrete_joint_entropy",
     "discrete_multivar_joint_entropy",
