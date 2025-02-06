e49b8493f351a982a00934f0d5d5983eb8175b3f
Max Mynter <maxmynter@me.com>
2025-02-06 14:14:14 +0100

    Expose TE columns in wrangling package

--- analysis/scripts/futures/wrangling/__init__.py
+++ analysis/scripts/futures/wrangling/__init__.py
@@ -1,11 +1,6 @@
 """Expose wrangling functionality."""
 
-from .columns import Cols, InstrumentCols, Instruments
+from .columns import Cols, InstrumentCols, Instruments, TEColumns
 from .data_builder import FuturesDataBuilder
 
-__all__ = [
-    "Cols",
-    "FuturesDataBuilder",
-    "InstrumentCols",
-    "Instruments",
-]
+__all__ = ["Cols", "FuturesDataBuilder", "InstrumentCols", "Instruments", "TEColumns"]
