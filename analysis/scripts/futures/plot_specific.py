bbdc3b2c1d8f80d1e40e1751ec9114a4a339e4d5
Max Mynter <maxmynter@me.com>
2025-03-02 22:32:24 -0500

    Specific Analysis showing Raw - BS surros
    instead of only bs surro's.

--- analysis/scripts/futures/plot_specific.py
+++ analysis/scripts/futures/plot_specific.py
@@ -29,41 +29,52 @@ class SpecificConfig(TentCalcConfig):
 filename = generate_filename(SpecificConfig(), get_bootstrap_maximised_te)
 
 onto_co = [
-    TEColumns.get_bs_column_name(Columns.ES, Columns.CO),
-    TEColumns.get_bs_column_name(Columns.NK, Columns.CO),
-    TEColumns.get_bs_column_name(Columns.VG, Columns.CO),
-    TEColumns.get_bs_column_name(Columns.HS, Columns.CO),
+    (Columns.ES, Columns.CO),
+    (Columns.NK, Columns.CO),
+    (Columns.VG, Columns.CO),
+    (Columns.HS, Columns.CO),
 ]
 
 vg_hi = [
-    TEColumns.get_bs_column_name(Columns.VG, Columns.HS),
-    TEColumns.get_bs_column_name(Columns.HS, Columns.VG),
+    (Columns.VG, Columns.HS),
+    (Columns.HS, Columns.VG),
 ]
 
 
 def plot_specific_te_pairs(
-    df: pl.DataFrame, onto_co: list[str], vg_hi: list[str], filename="specific_te.png"
+    df: pl.DataFrame,
+    onto_co: list[tuple[str, str]],
+    vg_hi: list[tuple[str, str]],
+    filename="specific_te.png",
 ):
     """Plot specific TE pairs."""
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
 
     for col in onto_co:
-        ax1.plot(df[Columns.Date], df[col], label=col.split("_")[2][:3], alpha=0.7)
+        raw = TEColumns.get_te_column_name(col[0], col[1])
+        bs = TEColumns.get_bs_column_name(col[0], col[1])
+        ax1.plot(
+            df[Columns.Date],
+            df[raw] - df[bs],
+            label=bs.split("_")[2][:3],
+            alpha=0.7,
+        )
     ax1.legend()
     ax1.grid(True, alpha=0.3)
 
     for col in vg_hi:
+        raw = TEColumns.get_te_column_name(col[0], col[1])
+        bs = TEColumns.get_bs_column_name(col[0], col[1])
         ax2.plot(
             df[Columns.Date],
-            df[col],
-            label=f"{col.split('_')[2][:3]} → {col.split('_')[2][-3:]}",
+            df[raw] - df[bs],
+            label=f"{bs.split('_')[2][:3]} → {bs.split('_')[2][-3:]}",
             alpha=0.7,
         )
     ax = plt.gca()
     ax.xaxis.set_major_locator(plt.MaxNLocator(6))
     plt.xticks(rotation=45)
 
-    ax2.set_title("Bidirectional Information Flow between EuroStoxx and Hang Seng")
     ax2.legend()
     ax2.grid(True, alpha=0.3)
 
@@ -87,6 +98,5 @@ if __name__ == "__main__":
     cols = []
     cols.extend(onto_co)
     cols.extend(vg_hi)
-    df = df.select([Columns.Date, *cols])
 
     plot_specific_te_pairs(df, onto_co, vg_hi, "specific_te_covid.png")
