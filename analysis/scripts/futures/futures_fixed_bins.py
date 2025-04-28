f012d15a63b654236f8bfa16850820e0d4b5d2f0
Max Mynter <maxmynter@me.com>
2025-04-28 12:02:48 -0400

    Calculate TE for fixed bins

--- analysis/scripts/futures/futures_fixed_bins.py
+++ analysis/scripts/futures/futures_fixed_bins.py
@@ -4,11 +4,16 @@ import numpy as np
 from futures_constants import (
     DATA_PATH,
     MIN_TICKS_PER_DAY,
-    PLOT_PATH,
     RETURNS_DATA,
     TentCalcConfig,
+    generate_filename,
 )
-from wrangling import Cols, FuturesDataBuilder, InstrumentCols, TEColumns
+from futures_main import (
+    build_rolling_pairwise_measure_df,
+    get_transfer_entropy_surros_for_bins,
+)
+from numpy.typing import NDArray
+from wrangling import Cols, FuturesDataBuilder
 
 config = TentCalcConfig()
 
@@ -24,6 +29,7 @@ return_cols = [
 date_return_cols = [Cols.Date, *return_cols]
 
 if __name__ == "__main__":
+    analysis_cols = Cols.get_all_instruments()
     futures = FuturesDataBuilder.load(RETURNS_DATA)
 
     df_builder = (
@@ -38,10 +44,36 @@ if __name__ == "__main__":
     lowest, highest = df[return_cols].to_numpy().min(), df[return_cols].to_numpy().max()
 
     for n_bins in N_BINS:
-        bins = np.linspace(lowest, highest, num=n_bins + 1)
-        bins_sym_about_zero = np.concatenate(
+        print(f"=== {n_bins} BINS ===")
+        bins: NDArray[np.float64] = np.linspace(lowest, highest, num=n_bins + 1)
+        bins_sym_about_zero: NDArray[np.float64] = np.concatenate(
             [
                 np.linspace(lowest, 0, num=n_bins // 2 + 1),
                 np.linspace(0, highest, num=n_bins // 2 + 1)[1:],
             ]
         )
+        fixed_n_bins_filename = generate_filename(
+            config, f"fixed_{n_bins}_bins_over_range"
+        )
+        bin_tents = build_rolling_pairwise_measure_df(
+            df,
+            analysis_cols,
+            lambda src, tgt, df, bins=bins: get_transfer_entropy_surros_for_bins(
+                src, tgt, df, bins
+            ),
+        )
+        bin_tents.write_csv(DATA_PATH / f"{fixed_n_bins_filename}.csv")
+        fixed_n_sym_bins_filename = generate_filename(
+            config, f"fixed_{n_bins}_bins_sym_abt_0"
+        )
+        sym_bin_tents = build_rolling_pairwise_measure_df(
+            df,
+            analysis_cols,
+            lambda src,
+            tgt,
+            df,
+            sym_bins=bins_sym_about_zero: get_transfer_entropy_surros_for_bins(
+                src, tgt, df, sym_bins
+            ),
+        )
+        sym_bin_tents.write_csv(DATA_PATH / f"{fixed_n_sym_bins_filename}.csv")
